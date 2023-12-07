e0f007111e4c81495858248bad71a5e139655eb8
Crowdbotics <team@crowdbotics.com>
2023-12-07 16:10:26 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "snakes-tail-44888",
+  url: "https://snakes-tail-44888.botics.co",
+  api: "https://snakes-tail-44888.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
